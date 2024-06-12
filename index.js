835883bb06e0807595b39edeb85e52946e17a9e9
AwsomeSauci <progimp31@gmail.com>
2024-06-12 22:11:30 +0300

    123

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ bot.on("callback_query", function (query) {
         bot.answerCallbackQuery(query.id, "Sorry, '" + query.game_short_name + "' is not available.");
     } else {
         queries[query.id] = query;
-        let gameurl = "https://awsomesauci.github.io/CryptoTest/";
+        let gameurl = "https://cj84452.tw1.ru/";
         bot.answerCallbackQuery({
             callback_query_id: query.id,
             url: gameurl
